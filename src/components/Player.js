b8c163a3198f3e9b0ee8aa044ad3219e2a54682c
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-01-12 16:09:57 +0100

    Added uuid package

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
   return (
@@ -9,7 +15,11 @@ const Player = () => {
         <p>End Time</p>
       </div>
 
-      <div className='play-control'></div>
+      <div className='play-control'>
+        <FontAwesomeIcon className='prev' size='2x' icon={faAngleLeft} />
+        <FontAwesomeIcon className='play' size='2x' icon={faPlay} />
+        <FontAwesomeIcon className='next' size='2x' icon={faAngleRight} />
+      </div>
     </div>
   );
 };
