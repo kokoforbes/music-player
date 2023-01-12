b638b67e3c3e4a25dc91976567792f1e666ffb16
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-01-12 20:52:17 +0100

    Updated styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from "react";
+import "./styles/app.scss";
 import Song from "./components/Song";
 import Player from "./components/Player";
+import data from "./utils";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className='App'>
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
