6d9d91e62856dd0ef6e392855783c64cd8f36716
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-01-14 16:28:54 +0100

    Implemented changing of songs

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,17 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className='library'>
       <h2>library</h2>
       <div className='library-songs'>
         {songs.map((song) => (
-          <LibrarySong song={song} key={song.id} />
+          <LibrarySong
+            song={song}
+            key={song.id}
+            setCurrentSong={setCurrentSong}
+          />
         ))}
       </div>
     </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, setCurrentSong }) => {
+  const songSelectHandler = async () => {
+    await setCurrentSong(song);
+  };
+
   return (
-    <div className='library-song'>
+    <div onClick={songSelectHandler} className='library-song'>
       <img src={song.cover} alt={song.name} />
 
       <div className='song-description'>
